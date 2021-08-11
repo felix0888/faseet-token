008db4e0adf6a6cc35c434ba94c869dcc9d4fa21
felixnan88 <felixnan88@gmail.com>
2021-08-11 16:23:33 -0400

    [FAT-3] finished deploy / test scripts, gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const INFURA_PROJECT_ID = "";
+const ROPSTEN_PRIVATE_KEY= "";
+
 module.exports = {
   solidity: "0.7.6",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  },
+  gasReporter: {
+    enabled: true
+  }
 };
--- test/FAssetToken.test.js
+++ test/FAssetToken.test.js
@@ -23,10 +23,8 @@ describe("FAssetToken", function () {
 
     it("should update the total supply, balance and vote power of the owner", async function() {
       totalSupply = await fAsset.totalSupply();
-      ownerBalance = await fAsset.balanceOfAt(owner.address, blockNumber);
-      ownerVotePower = await fAsset.votePowerOfAt(owner.address, blockNumber);
-      expect(ownerBalance).to.equal(totalSupply);
-      expect(ownerVotePower).to.equal(totalSupply);
+      expect(await fAsset.balanceOfAt(owner.address, blockNumber)).to.equal(totalSupply);
+      expect(await fAsset.votePowerOfAt(owner.address, blockNumber)).to.equal(totalSupply);
     });
   });
 
@@ -34,10 +32,8 @@ describe("FAssetToken", function () {
     it("should update balance and vote power of the minter", async function() {
       await fAsset.connect(bob).mint(100);
       blockNumber++;
-      balance = await fAsset.balanceOfAt(bob.address, blockNumber);
-      votePower = await fAsset.votePowerOfAt(bob.address, blockNumber);
-      expect(balance).to.equal(100);
-      expect(votePower).to.equal(100);
+      expect(await fAsset.balanceOfAt(bob.address, blockNumber)).to.equal(100);
+      expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(100);
     });
   });
 
@@ -48,28 +44,22 @@ describe("FAssetToken", function () {
       await fAsset.connect(bob).transfer(lucy.address, 20);
       blockNumber++;
 
-      balanceBob = await fAsset.balanceOfAt(bob.address, blockNumber);
-      votePowerBob = await fAsset.votePowerOfAt(bob.address, blockNumber);
-      balanceLucy = await fAsset.balanceOfAt(lucy.address, blockNumber);
-      votePowerLucy = await fAsset.votePowerOfAt(lucy.address, blockNumber);
-      expect(balanceBob).to.equal(80);
-      expect(votePowerBob).to.equal(80);
-      expect(balanceLucy).to.equal(20);
-      expect(votePowerLucy).to.equal(20);
+      expect(await fAsset.balanceOfAt(bob.address, blockNumber)).to.equal(80);
+      expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(80);
+      expect(await fAsset.balanceOfAt(lucy.address, blockNumber)).to.equal(20);
+      expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(20);
     });
   });
 
   describe("delegate", function() {
-    context("with invalid address", function() {
-      it("should be reverted with proper error message", async function() {
+    context("with invalid delegator", function() {
+      it("should be reverted for delegator same with owner", async function() {
         await expect(
           fAsset.connect(bob).delegate(bob.address, 10)
         ).to.be.revertedWith("FAsset: invalid delegation address.");
       });
-    });
 
-    context("with delegator behind 5th", function() {
-      it("should be reverted with proper error message", async function() {
+      it("should be reverted for the delegator behind 5th", async function() {
         await fAsset.connect(bob).delegate(lucy.address, 10);
         await fAsset.connect(bob).delegate(ed.address, 10);
         await fAsset.connect(bob).delegate(addr1.address, 10);
@@ -89,7 +79,7 @@ describe("FAssetToken", function () {
         ).to.be.revertedWith("FAsset: invalid delegation amount.");
       });
 
-      it("should be reverted when delegation percentage is greater than the remaining", async function() {
+      it("should be reverted with delegation percentage greater than remaining", async function() {
         await fAsset.connect(bob).delegate(lucy.address, 60);
         await expect(
           fAsset.connect(bob).delegate(ed.address, 50)
@@ -104,25 +94,86 @@ describe("FAssetToken", function () {
         await fAsset.connect(bob).delegate(lucy.address, 20);
         blockNumber++;
 
-        balanceBob = await fAsset.balanceOfAt(bob.address, blockNumber);
-        votePowerBob = await fAsset.votePowerOfAt(bob.address, blockNumber);
-        balanceLucy = await fAsset.balanceOfAt(lucy.address, blockNumber);
-        votePowerLucy = await fAsset.votePowerOfAt(lucy.address, blockNumber);
-        expect(balanceBob).to.equal(200);
-        expect(votePowerBob).to.equal(160);
-        expect(balanceLucy).to.equal(0);
-        expect(votePowerLucy).to.equal(40);
+        expect(await fAsset.balanceOfAt(bob.address, blockNumber)).to.equal(200);
+        expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(160);
+        expect(await fAsset.balanceOfAt(lucy.address, blockNumber)).to.equal(0);
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(40);
       });
     });
   });
 
   describe("special test scenarios", function() {
     context("with test scenario #1", function() {
-
+      it("should retrieve correct vote power values of an address at block", async function() {
+        // Bob has 20 tokens: vote power 20, Lucy has 10 tokens: vote power 10, Ed has no tokens: vote power 0.
+        // votePowerOfAt(bob, 9) -> 20
+        // votePowerOfAt(lucy, 9) -> 10
+        // votePowerOfAt(ed, 9) -> 0
+        await fAsset.connect(owner).transfer(bob.address, 20);
+        blockNumber++;
+        await fAsset.connect(owner).transfer(lucy.address, 10);
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(20);
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(10);
+        expect(await fAsset.votePowerOfAt(ed.address, blockNumber)).to.equal(0);
+        // Bob delegates 50% of voting power to Lucy and 25% to Ed.
+        // Now Bob has vote power 5, Lucy has vote power 20, Ed has vote power 5
+        // votePowerOfAt(bob, 9) -> 20
+        // votePowerOfAt(lucy, 9) -> 5
+        // votePowerOfAt(ed, 9) -> 0
+        blockNumber++;
+        await fAsset.connect(bob).delegate(lucy.address, 50);
+        blockNumber++;
+        await fAsset.connect(bob).delegate(ed.address, 25);
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(5);
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(20);
+        expect(await fAsset.votePowerOfAt(ed.address, blockNumber)).to.equal(5);
+        // Bob receives 16 tokens
+        // votePowerOfAt(Bob, 13) -> 9
+        // votePowerOfAt(Lucy, 13) -> 28
+        // votePowerOfAt(Ed, 13) -> 9
+        blockNumber++;
+        await fAsset.connect(owner).transfer(bob.address, 16);
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(9);
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(28);
+        expect(await fAsset.votePowerOfAt(ed.address, blockNumber)).to.equal(9);
+        // Lucy delegates 100% of vote power to Ed (demonstrage 1 level delegation)
+        // votePowerOfAt(Bob, 16) -> 9
+        // votePowerOfAt(Lucy, 16) -> 18
+        // votePowerOfAt(Ed, 16) -> 19
+        blockNumber++;
+        await fAsset.connect(lucy).delegate(ed.address, 100);
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(9);
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(18);
+        expect(await fAsset.votePowerOfAt(ed.address, blockNumber)).to.equal(19);
+      });
     });
 
-    context("with test scenario #2", function() {
-      
+    context("with test scenario #2", async function() {
+      it("should retrieve correct vote power values of an address at block", async function() {
+        // Bob balance: 10, delegate none, Lucy balance: 0
+        await fAsset.connect(owner).transfer(bob.address, 10);
+        // validate: votePower for Bob: 10
+        // validate: votePower for Lucy: 0
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(bob.address, blockNumber)).to.equal(10);
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(0);
+        // Bob delegates 50% to Lucy
+        blockNumber++;
+        await fAsset.connect(bob).delegate(lucy.address, 50);
+        // validate votePower of Lucy: 5
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(5);
+        // Bob receives 10 tokens
+        blockNumber++;
+        await fAsset.connect(owner).transfer(bob.address, 10);
+        // validate vote power of lucy: 10
+        blockNumber++;
+        expect(await fAsset.votePowerOfAt(lucy.address, blockNumber)).to.equal(10);
+      });
     });
   });
 });
